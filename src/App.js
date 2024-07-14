bc51b963fca21085368e6dbbf4d711a3e14a8ccf
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-07-14 09:26:16 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import Home from "./pages/home/Home";
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import List from './pages/list/List'
+import Single from './pages/single/Single'
+import New from './pages/new/New'
+
+import {
+  BrowserRouter,
+  Route,
+  Routes
+}  from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
+    <div className="App"> 
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home/>} />
+            <Route path='login' element={<Login/>} />
+            <Route path='users'>
+              <Route index element={<List/>} />
+              <Route path=':userId' element={<Single/>} />
+              <Route path='new' element={<New/>} />
+            </Route>
+
+            <Route path='products'>
+              <Route index element={<List/>} />
+              <Route path=':productId' element={<Single/>} />
+              <Route path='new' element={<New/>} />
+            </Route>
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
